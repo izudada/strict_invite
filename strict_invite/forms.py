a27a0b671c2b2664f619eb71eded337e3e98431b
Anthony Udeagbala <tonyudeagbala@gmail.com>
2024-07-21 01:53:12 +0100

    update views

--- strict_invite/forms.py
+++ strict_invite/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 from .models import User
 
+
 class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label='First Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    last_name = forms.CharField(
+        label='Lasr Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    phone_number = forms.CharField(
+        label='Phone Number',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'phone_number', 'email']
